9dfbfd0b48d4ae63f373c854f4eece1ca8947990
alx-yng <alexyoung0119@gmail.com>
2024-04-02 22:27:33 -0500

    feat: header v1

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -6,23 +6,26 @@ import ModeToggle from "@/components/ModeToggle";
 
 const Header = () => {
   return (
-    <header className='bg-background flex justify-between items-center tracking-wider py-4 fixed w-full px-8 md:px-12 lg:px-16'>
-      <h1 className='font-semibold'>KIWI</h1>
-      <nav>
-        <ul className='flex gap-8 md:gap-12 lg:gap-16 items-center'>
-          {Links.map((link) => (
-            <NavLink key={link.name} name={link.name} href={link.href} />
-          ))}
-          <li>
-            <Button
-              variant='outline'
-              className='hover:scale-105 transition-transform duration-300 border-primary font-bold'
-            >
-              Join Now
-            </Button>
-          </li>
-        </ul>
-      </nav>
+    <header className='fixed w-full py-4 px-8 md:px-12 lg:px-16'>
+      <div className='bg-background flex justify-between items-center tracking-wider'>
+        <h1 className='font-semibold'>KIWI</h1>
+        <nav>
+          <ul className='flex gap-8 md:gap-12 lg:gap-16 items-center'>
+            {Links.map((link) => (
+              <NavLink key={link.name} name={link.name} href={link.href} />
+            ))}
+            <li>
+              <Button
+                variant='outline'
+                className='hover:scale-105 transition-transform duration-300 border-primary font-bold'
+              >
+                Join Now
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <hr className='border-t border-primary mt-4' />
     </header>
   );
 };
