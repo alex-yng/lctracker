75d89b6d4a6e16b246c531b7bb8285854bc3adf8
alx-yng <alexyoung0119@gmail.com>
2024-04-03 00:49:31 -0500

    feat: add smile if username is mine

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ import { Progress } from "@/components/ui/progress";
 import { Skeleton } from "@/components/ui/skeleton";
 import { userData } from "@/const";
 import fetchStats from "@/lib/fetchStats";
+import { ReloadIcon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [username, setUsername] = useState("alex");
+  const [username, setUsername] = useState("alicks111");
   const [loading, setLoading] = useState(true);
-  const [userData, setUserData] = useState({ userName: "." } as userData);
+  const [userData, setUserData] = useState({} as userData);
 
   const previewStats = async () => {
     setLoading(true);
@@ -60,22 +61,33 @@ const Home = () => {
               placeholder='username'
               onChange={(e) => setUsername(e.target.value)}
             />
-            <Button onClick={previewStats}>Preview</Button>
+            {loading ? (
+              <Button disabled>
+                <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+                Please wait
+              </Button>
+            ) : (
+              <Button onClick={previewStats}>Preview</Button>
+            )}
           </div>
           {loading ? (
             <div className='flex flex-col gap-4 w-full px-2'>
               <Skeleton className='w-36 h-8' />
-              <Skeleton className='w-24 h-4' />
-              <Skeleton className='w-full h-4' />
-              <Skeleton className='w-28 h-4' />
-              <Skeleton className='w-full h-4' />
-              <Skeleton className='w-20 h-4' />
-              <Skeleton className='w-full h-4' />
+              <Skeleton className='w-24 h-5' />
+              <Skeleton className='w-full h-5' />
+              <Skeleton className='w-28 h-5' />
+              <Skeleton className='w-full h-5' />
+              <Skeleton className='w-20 h-5' />
+              <Skeleton className='w-full h-5' />
             </div>
           ) : (
             <>
               <div className='flex flex-col gap-4 w-full px-2'>
-                <h1 className='text-4xl'>{userData.userName}</h1>
+                <h1 className='text-4xl'>
+                  {username === "alicks111"
+                    ? "alex :)"
+                    : `${userData.userName}`}
+                </h1>
                 <div className='flex flex-col gap-2'>
                   <p>Easys: {userData.easySolved}</p>
                   <Progress
