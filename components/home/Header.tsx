ac3dd6e603a35c4733862e2890a35e768f9048c5
alx-yng <alexyoung0119@gmail.com>
2024-04-02 21:41:28 -0500

    feat: theme toggle

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import NavLink from "@/components/home/NavLink";
 import { headerLinks as Links } from "@/const";
+import ModeToggle from "@/components/ModeToggle";
 
 const Header = () => {
   return (
-    <header className='container bg-background flex justify-between items-center py-4 fixed w-full'>
+    <header className='bg-background flex justify-between items-center py-4 fixed w-full px-8 md:px-12 lg:px-16'>
       <h1>Header</h1>
       <nav>
-        <ul className='flex gap-16 items-center'>
+        <ul className='flex gap-8 md:gap-12 lg:gap-16 items-center'>
           {Links.map((link) => (
             <NavLink key={link.name} name={link.name} href={link.href} />
           ))}
+          <li className='hover:scale-110 transition-transform duration-300'>
+            <ModeToggle />
+          </li>
           <li>
             <Button
               variant='outline'
